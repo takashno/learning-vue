2396f54a1e4732b11dfcd458e4dbf7b564006c0b
高島望 <nozomu.takashima@gmail.com>
2018-10-09 01:21:45 +0900

    動作確認用のAPIとして簡単に導入できるLoopbackを追加。

--- src/components/pages/ExampleTable.vue
+++ src/components/pages/ExampleTable.vue
@@ -1,40 +1,78 @@
 <template comments>
   <main class="col-md-9" style="padding:30px;">
-    <div class="row">
-      <div class="col-md-12 text-left">
-        <h2>Example&nbsp;Table</h2>
-        <b-table bordered striped hover :items="items"></b-table>
+    <article>
+      <div class="row">
+        <div class="col-md-12 text-left">
+          <h2>Example&nbsp;Table</h2>
+          <small>
+            Bootstrap4のTableComponentsを利用したテーブルの例。
+          </small>
+          <b-table sortable border striped hover :items="items" :fields="fields"></b-table>
+        </div>
       </div>
-    </div>
+    </article>
   </main>
 </template>
 
 <script>
-const items = [
-  { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-  { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-  {
-    isActive: false,
-    age: 89,
-    first_name: 'Geneva',
-    last_name: 'Wilson',
-    _rowVariant: 'danger'
-  },
-  {
-    isActive: true,
-    age: 40,
-    first_name: 'Thor',
-    last_name: 'Macdonald',
-    _cellVariants: { isActive: 'success', age: 'info', first_name: 'warning' }
-  },
-  { isActive: false, age: 29, first_name: 'Dick', last_name: 'Dunlap' }
-]
 
 export default {
   name: 'ExampleTable',
-  data () {
+  data() {
     return {
-      items: items
+      // Note 'isActive' is left out and will not appear in the rendered table
+      fields: {
+        last_name: {
+          label: 'Person last name',
+          sortable: true
+        },
+        first_name: {
+          label: 'Person first name',
+          sortable: false
+        },
+        foo: {
+          // This key overrides `foo`!
+          key: 'age',
+          label: 'Person age',
+          sortable: true
+        },
+        city: {
+          key: 'address.city'
+        },
+        'address.country': {
+          label: 'Country'
+        }
+      },
+      items: [
+        {
+          isActive: true,
+          age: 40,
+          first_name: 'Dickerson',
+          last_name: 'Macdonald',
+          address: {country: 'USA', city: 'New York'}
+        },
+        {
+          isActive: false,
+          age: 21,
+          first_name: 'Larsen',
+          last_name: 'Shaw',
+          address: {country: 'Canada', city: 'Toronto'}
+        },
+        {
+          isActive: false,
+          age: 89,
+          first_name: 'Geneva',
+          last_name: 'Wilson',
+          address: {country: 'Australia', city: 'Sydney'}
+        },
+        {
+          isActive: true,
+          age: 38,
+          first_name: 'Jami',
+          last_name: 'Carney',
+          address: {country: 'England', city: 'London'}
+        }
+      ]
     }
   },
   mounted () {
